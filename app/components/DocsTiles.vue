f53d88de7a5f6a7b6ad1858f3b1e2c2b5429f79e
Marshall Thompson <marshall@creativeideal.net>
2025-05-01 22:35:31 -0600

    show text

--- app/components/DocsTiles.vue
+++ app/components/DocsTiles.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
+import type { AuthCollectionItem, FeathersCollectionItem, PinionCollectionItem } from '@nuxt/content';
+
 defineProps<{
   superTitle?: string
   title: string
-  pages: PinionCollectionItem[] | AuthCollectionItem[] | FeathersCollectionItem[]
+  pages: (PinionCollectionItem | AuthCollectionItem | FeathersCollectionItem)[]
 }>()
 </script>
 
 <template>
-  <div>
-    <Text v-if="superTitle" size="2xl">{{ superTitle }}</Text>
-    <Text size="3xl">{{ title }}</Text>
-    <div v-if="pages" class="pt-6 px-3 lg:px-6 mx-auto max-w-5xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 bg-base-content text-base-100 mt-24">
+  <Flex col class="bg-base-content text-base-100 mt-24 mx-auto max-w-5xl">
+    <Flex col class="pt-3">
+      <Text center v-if="superTitle">{{ superTitle }}</Text>
+      <Text center size="3xl">{{ title }}</Text>
+    </Flex>
+
+    <div v-if="pages" class="pt-6 px-3 lg:px-6  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
       <NuxtLink class="card" :to="page.path" v-for="page in pages" :key="page.id">
         <figure class="bg-base-100 aspect-video"></figure>
         <CardBody>
@@ -19,5 +24,5 @@ defineProps<{
         </CardBody>
       </NuxtLink>
     </div>
-  </div>
+  </Flex>
 </template>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
-import antfu from '@antfu/eslint-config'
+// import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  antfu().override('antfu/node/rules', {
-    rules: {
-      'node/prefer-global/buffer': 'off',
-      'curly': ['error', 'all'],
-      'brace-style': ['error', 'stroustrup', { allowSingleLine: false }],
-      'unused-imports/no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_', caughtErrors: 'none' }],
-    },
-  }),
+  // antfu().override('antfu/node/rules', {
+  //   rules: {
+  //     'node/prefer-global/buffer': 'off',
+  //     'curly': ['error', 'all'],
+  //     'brace-style': ['error', 'stroustrup', { allowSingleLine: false }],
+  //     'unused-imports/no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_', caughtErrors: 'none' }],
+  //   },
+  // }),
 )
