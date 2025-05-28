5fcfa39fef139ea973d8d942803b9955c06c9bf4
Marshall Thompson <marshall@creativeideal.net>
2025-05-27 18:57:49 -0700

    add help page

---
+++ app/pages/help.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'page',
+})
+
+</script>
+
+<template>
+  <div class="bg-[url('/img/top_background.svg')] bg-no-repeat bg-cover bg-center text-base-content max-w-screen overflow-x-hidden">
+    <div class="relative mx-auto max-w-[82rem] lg:drawer-open pt-16 px-4">
+      <Titles title="Help Center" sub-title="Find answers to your questions" class="py-24"/>
+    </div>
+    <div class="h-64"></div>
+  </div>
+  <div class="bg-base-200 min-h-screen max-w-[82rem] mx-auto -mt-64 -mb-64 rounded-4xl p-6 pt-12 lg:p-12">
+    <Discord />
+  </div>
+</template>
