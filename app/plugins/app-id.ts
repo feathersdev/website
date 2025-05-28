732c661f86a6dd376a3d620c696fc3fe0dfc19f8
daffl <daff@neyeon.com>
2025-05-27 20:44:02 -0700

    Replace your-app-id in docs with actual app id

---
+++ app/plugins/app-id.ts
@@ -0,0 +1,51 @@
+const ONE_DAY = 24 * 60 * 60 * 1000
+
+function getAppId() {
+  const urlParams = new URLSearchParams(window.location.search)
+  const urlAppId = urlParams.get('appId')
+
+  if (urlAppId && urlAppId.startsWith('did:key:')) {
+    localStorage.setItem('appId', urlAppId)
+    localStorage.setItem('lastAccess', Date.now().toString())
+  }
+
+  const lastAccess = Number.parseInt(localStorage.getItem('lastAccess') || '0')
+
+  // Check if lastAccess was more than 1 day ago, we don't want to store appId longer than that
+  if (lastAccess + ONE_DAY < Date.now()) {
+    localStorage.removeItem('appId')
+    localStorage.removeItem('lastAccess')
+  }
+
+  return localStorage.getItem('appId') || null
+}
+
+export default defineNuxtPlugin(() => {
+  const router = useRouter()
+  
+  router.afterEach(async () => {
+    await nextTick()
+
+    function updatePage() {
+      const appId = getAppId()
+      
+      if (appId) {
+        document.body.querySelectorAll('span').forEach((el) => {
+          // Process only direct text nodes, not text in child elements
+          for (const node of el.childNodes) {
+            if (node.nodeType === Node.TEXT_NODE && node.textContent && node.textContent.includes('<your-app-id>')) {
+              node.textContent = node.textContent.replace(/<your-app-id>/g, appId);
+            }
+          }
+        })
+      }
+    }
+
+    updatePage()
+
+    const nuxtApp = useNuxtApp()
+    nuxtApp.hook('page:finish', () => {
+      updatePage()
+    })
+  })
+})
