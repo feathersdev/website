708df5adbc48fbb6db974e6736f6f9e6e2626a9a
Marshall Thompson <marshall@creativeideal.net>
2025-05-27 20:53:39 -0700

    enable cloudflare build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'cloudflare_pages',
+  },
 
   modules: [
     '@nuxt/content',
