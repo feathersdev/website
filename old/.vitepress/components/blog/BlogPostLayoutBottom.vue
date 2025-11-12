f6bd9107206a78514aa1c774cb55ad4389096c09
daffl <daff@neyeon.com>
2025-11-12 11:56:13 -0800

    feat: Move to Astro Starlight

--- old/.vitepress/components/blog/BlogPostLayoutBottom.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import BlogPostLinks from './BlogPostLinks.vue'
-</script>
-
-<template>
-  <BlogPostLinks inside-doc />
-</template>
--- old/blog/open-source.md
+++
@@ -1,6 +0,0 @@
----
-layout: home
-title: Blog
----
-
-<BlogListings category="Open Source" />
